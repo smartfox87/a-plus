5e98073173784b7a190b8b8965ce95cc02bbbd45
Andrei Dyminski <smoke59@mail.ru>
2017-03-14 20:22:15 +0300

    переделал автоматизацию и сборщик под gulp 4.0

--- gulpfile.js
+++ gulpfile.js
@@ -3,50 +3,17 @@
 var gulp = require("gulp");
 var less = require("gulp-less");
 var plumber = require("gulp-plumber");
+var notify = require('gulp-notify');
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var mqpacker = require("css-mqpacker");
-var server = require("browser-sync");
+var server = require("browser-sync").create();
 var minify = require("gulp-csso");
 var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var svgstore = require("gulp-svgstore");
 var svgmin = require("gulp-svgmin");
-var run = require("run-sequence");
 var del = require("del");
-
-gulp.task("style:work", function () {
-    gulp.src("less/style.less")
-        .pipe(plumber())
-        .pipe(less())
-        .pipe(postcss([
-            autoprefixer({
-                browsers: [
-                    "last 1 version",
-                    "last 2 Chrome versions",
-                    "last 2 Firefox versions",
-                    "last 2 Opera versions",
-                    "last 2 Edge versions"
-                ]
-            }),
-            mqpacker({
-                sort: true
-            })
-        ]))
-        .pipe(gulp.dest("css"))
-        .pipe(minify())
-        .pipe(rename("style.min.css"))
-        .pipe(gulp.dest("css"))
-        .pipe(server.stream());
-});
-
-gulp.task("serve:work", ["style:work"], function () {
-    server.init({
-        server: "."
-    });
-    gulp.watch("less/**/*.less", ["style:work"]).on("change", server.reload);
-    gulp.watch("*.html").on("change", server.reload);
-});
 
 gulp.task("clean", function () {
     return del("build");
@@ -64,9 +31,14 @@ gulp.task("copy", function () {
         .pipe(gulp.dest("build"));
 });
 
+gulp.task("copyhtml", function () {
+    return gulp.src("*.html")
+        .pipe(gulp.dest("build"));
+})
+
 gulp.task("style", function () {
-    gulp.src("less/style.less")
-        .pipe(plumber())
+    return gulp.src("less/*.less")
+        .pipe(plumber({errorHandler: notify.onError()}))
         .pipe(less())
         .pipe(postcss([
             autoprefixer({
@@ -97,31 +69,22 @@ gulp.task("images", function () {
         .pipe(gulp.dest("build/img"));
 });
 
-gulp.task("symbols", function () {
-    return gulp.src("build/img/icons/*.svg")
-        .pipe(svgmin())
-        .pipe(svgstore({
-            inlineSvg: true
-        }))
-        .pipe(rename("symbols.svg"))
-        .pipe(gulp.dest("build/img"));
+gulp.task('watch', function () {
+    gulp.watch('less/*.less', gulp.series('style'));
+    gulp.watch('*.html', gulp.series('copyhtml'));
 });
 
 gulp.task("serve", function () {
     server.init({
-        server: "build"
+        server: "build",
+        index: "index.html"
     });
-    gulp.watch("less/**/*.less", ["style"]);
-    gulp.watch("*.html").on("change", server.reload);
+    server.watch("less/*.less", gulp.series("style")).on("change", server.reload);
+    server.watch("build/*.html").on("change", server.reload);
 });
 
-gulp.task("build", function (fn) {
-    run(
-        "clean",
-        "copy",
-        "style",
-        "images",
-        "symbols",
-        fn
-    );
-});
+gulp.task("dev",
+    gulp.series("clean", "copy", "style", "images",
+        gulp.parallel('watch', 'serve')));
+
+
